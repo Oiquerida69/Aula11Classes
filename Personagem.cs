8d70df621b04cdde4041f69dad1e891b811a1d53
Wendell Mickael Cardoso <135742936+Oiquerida69@users.noreply.github.com>
2023-07-25 17:19:22 -0300

    Add files via upload

---
+++ Personagem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula12Classes
+{
+    internal class Personagem
+    {
+        //variaveis (atributos) "dentro de uma classe"
+        public string nome;
+        public string raça;
+        public int poder;
+
+        //funções (métodos) "dentro de uma classe"
+        public void mostra()
+        {
+            Console.WriteLine($"Seu personagem é da(o) raça : {raça} , tem o nivel de poder :{poder} e o nome dele(a) é : {nome} ");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Aula12Classes;
+
+
+Personagem guerreiro = new Personagem();
+guerreiro.nome = "Luz";
+guerreiro.raça = "humano";
+guerreiro.poder = 1000;
+
+guerreiro.mostra();
+Console.ReadKey();
+
+
+Personagem mago = new Personagem();
+mago.nome = "Dark";
+mago.raça = "Monge";
+mago.poder = 1200;
+
+mago.mostra();
+Console.ReadKey();
+
+
+Personagem bruxo = mago;
+bruxo.nome = "Hermione";
+
+bruxo.mostra();
+Console.ReadKey();
+
+
+Personagem Bardo = new Personagem();
+Bardo.nome = "Bethoven";
+Bardo.raça = "Alien";
+Bardo.poder = 1770;
+
+Bardo.mostra();
+Console.ReadKey();
